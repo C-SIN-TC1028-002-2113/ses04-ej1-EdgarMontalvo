2a41dc5c0f8bdfbfee2f3d738fb93305df5c77c0
EdgarMontalvo <A01742343@itesm.mx>
2021-08-20 16:51:18 +0000

    maximo1

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -6,7 +6,7 @@ def main():
 
     if num1>= num2 and num1>=num3:
         print(num1)
-    elif num1>=num2>=num3:
+    elif num2>=num1 and num2>=num3:
         print(num2)
     else:
         print(num3)
