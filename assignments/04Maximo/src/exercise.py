53e732ccdff450a02aa1956914d503c7297fe2cf
EdgarMontalvo <A01742343@itesm.mx>
2021-08-20 16:48:21 +0000

    Maximo

--- assignments/04Maximo/src/exercise.py
+++ assignments/04Maximo/src/exercise.py
@@ -4,12 +4,12 @@ def main():
     num2 = int(input("Ingresa el segundo número: "))
     num3 = int(input("Ingresa el tercer número: "))
 
-    if num1>num2 and num1>num3:
-        print("El mayor numero es", num1)
-    elif num2>num3 and num2>3:
-        print("El mayor numero es", num2)
+    if num1>= num2 and num1>=num3:
+        print(num1)
+    elif num1>=num2>=num3:
+        print(num2)
     else:
-        print("El mayor es", num3)
+        print(num3)
     
 if __name__=='__main__':
 
