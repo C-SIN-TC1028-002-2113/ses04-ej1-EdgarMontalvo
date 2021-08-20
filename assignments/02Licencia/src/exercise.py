082e8e1be40f72037e2a763a23f8f9a158a8c08d
EdgarMontalvo <A01742343@itesm.mx>
2021-08-20 15:11:19 +0000

    SESION 4 EJERCICO EN CLASE

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    ident= input("¿Tienes identificación oficial? (s/n): ")
+    
+    if edad < 18 and ident == n
+       print("No cumples requisitos")
+    elif
+        print("Trámite de licencia concedido")
+    
+
+
 
 if __name__ == '__main__':
     main()
