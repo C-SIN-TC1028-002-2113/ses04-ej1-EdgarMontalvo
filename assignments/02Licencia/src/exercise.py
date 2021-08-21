42d2323accf20ed8ecb593ad25e5f057e5c7aca2
EdgarMontalvo <A01742343@itesm.mx>
2021-08-21 18:29:56 +0000

    licencia bien

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,19 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
     edad = int(input("Ingresa tu edad: "))
-    ident= input("¿Tienes identificación oficial? (s/n): ")
-   
-    if edad < 18: 
-        print("No cumples requisitos")
-    elif ident == "n":
-        print("No cumples requisitos")
-    elif ident == "s" :
-        print("Trámite de licencia concedido")
+
+    if edad>0:
+        if edad>=18:
+            ident = (input("¿Tienes identificación oficial? (s/n): "))
+            if ident == "s":
+                print("Trámite de licencia concedido")
+            elif ident == "n":
+                 print("No cumples requisitos")
+            else:
+                 print("Respuesta incorrecta")
+        else:
+            print("No cumples requisitos")
     else:
         print("Respuesta incorrecta")
 
-
-    
 
 
 if __name__ == '__main__':
